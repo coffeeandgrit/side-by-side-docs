fe2786cc87f306e9ac261cdb43508c9913e5dcc3
Ross <rosshinkley@gmail.com>
2015-10-26 13:32:34 -0400

    starting to look at why language isnt' coming across

--- templates/helpers/language.js
+++ templates/helpers/language.js
@@ -1,12 +1,15 @@
 var Handlebars = require('handlebars'),
+    logger = require('winston'),
+    util = require('util'),
     cheerio = require('cheerio');
 
 module.exports = exports = function() {
     Handlebars.registerHelper('documentation_language', function(language) {
+        logger.silly('language: ' + util.inspect(language));
         var $ = cheerio.load('<a></a>');
         $('a').attr('class', 'selection');
         $('a').attr('href', '#');
-        $('a').text(language);
+        //$('a').text(language);
         return $.html();
     });
 };
