ae1f3865a04f5016d4c6acf56337afa6400ef75a
Ross <rosshinkley@gmail.com>
2015-10-27 18:00:22 -0400

    propping up parts to run as cng bolton

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 var Promise = require('bluebird'),
-    _ = require('lodash'),
-    path = require('path'),
-    fs = Promise.promisifyAll(require('fs')),
-    util = require('util'),
     logger = require('winston'),
-    cheerio = require('cheerio'),
-    marked = require('marked'),
-    Prism = require('prismjs'),
-    vm = require('vm'),
+    path = require('path'),
     make = require('./lib/make'),
-    glob = Promise.promisify(require('glob'));
+    express = require("express"),
+    util = require("util"),
+    http = require("http");
 
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    level: 'silly',
-    colorize: true
-});
 
-make(__dirname, path.resolve(__dirname, 'output')).then(function(){
-    logger.info('done');
-});
+module.exports = exports  = function(app, server) {
+    var router = express.Router();
+    return Promise.cast()
+        .then(function(){
+            return make(path.resolve(__dirname, 'src'), path.resolve(__dirname, 'output'));
+        })
+        .then(function() {
+            router.use('/', express.static(path.resolve(__dirname, 'output')));
+            return router;
+        })
+};
+
+if (require.main === module) {
+    require('cng-standalone')(require('./package.json'), module.exports);
+}
